0f46092a6984e2fbf0f52d97b9f353fd1a6eda3b
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-08-22 13:36:37 -0500

    Initial commit.

---
+++ src/main/java/pruebatecnica/vueloscolombia/VueloscolombiaApplication.java
@@ -0,0 +1,13 @@
+package pruebatecnica.vueloscolombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueloscolombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueloscolombiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pruebatecnica/vueloscolombia/VueloscolombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebatecnica.vueloscolombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueloscolombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
