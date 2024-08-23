e4e170733d396d6e4b17630bba951fb9390184be
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-08-23 15:07:31 -0500

    Revert "Creacion Bean RestTemplate"
    This reverts commit f67ed0fc1ed07a74e9aa64d6a23a55fb8fba3f2f.

---
+++ src/main/java/pruebatecnica/vueloscolombia/Utils/RestTemplate/RestTemplate.java
@@ -0,0 +1,10 @@
+// package pruebatecnica.vueloscolombia.Utils.RestTemplate;
+
+// public class RestTemplate {
+    
+
+//     private RestTemplate restTemplate = new RestTemplate();
+
+//     private String url = "https://bitecingcom.ipage.com/testapi/avanzado.js"
+
+// }
